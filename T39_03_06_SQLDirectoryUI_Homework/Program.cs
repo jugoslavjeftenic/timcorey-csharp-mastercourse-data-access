87dc4e2ea1220afc8c3dcf628d33518d4a49557f
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-28 10:00:56 +0200

    SQLDirectoryUI_Homework

---
+++ T39_03_06_SQLDirectoryUI_Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace T39_03_06_SQLDirectoryUI_Homework
+{
+	// Build a simple database in SQL that holds People, Addresses, and Employers.
+	// Make sure it builds and that you can load and save data in C#.
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
