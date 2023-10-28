d9b9c1b0b71bab7db22a575d20eff66563589b18
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-28 12:32:27 +0200

    SQLDirectoryUI_Homework

--- T39_03_06_SQLDirectoryUI_Homework/Program.cs
+++ T39_03_06_SQLDirectoryUI_Homework/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace T39_03_06_SQLDirectoryUI_Homework
+﻿using Microsoft.Extensions.Configuration;
+using T39_03_05_SQLDirectoryDALibrary_Homework;
+
+namespace T39_03_06_SQLDirectoryUI_Homework
 {
 	// Build a simple database in SQL that holds People, Addresses, and Employers.
 	// Make sure it builds and that you can load and save data in C#.
@@ -7,7 +10,20 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			SqlCrud sql = new(GetConnectionString());
+			sql.CreatePerson();
+		}
+
+		private static string GetConnectionString(string connectionStringName = "Default")
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json");
+
+			var config = builder.Build();
+
+			return config.GetConnectionString(connectionStringName) ??
+				throw new Exception("Error connecting to DB.");
 		}
 	}
 }
