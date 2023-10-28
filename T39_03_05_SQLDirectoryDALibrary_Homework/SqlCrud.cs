1690fd94ecff406c330e30c0855f26f8186a51e0
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-10-28 12:32:07 +0200

    SQLDirectoryDALibrary_Homework

--- T39_03_05_SQLDirectoryDALibrary_Homework/SqlCrud.cs
+++ T39_03_05_SQLDirectoryDALibrary_Homework/SqlCrud.cs
@@ -1,6 +1,24 @@
-﻿namespace T39_03_05_SQLDirectoryDALibrary_Homework
+﻿using T39_03_05_SQLDirectoryDALibrary_Homework.Models;
+
+namespace T39_03_05_SQLDirectoryDALibrary_Homework
 {
 	public class SqlCrud
 	{
+		private readonly string _connectionString;
+		private readonly SqlDataAccess db = new();
+
+		public SqlCrud(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		// Create Person
+		public void CreatePerson(PersonModel? person = null)
+		{
+			person ??= new PersonModel();
+
+			string sql = "insert into dbo.People (FirstName, LastName) values (@FirstName, @LastName);";
+			db.SaveData(sql, new { person.FirstName, person.LastName }, _connectionString);
+		}
 	}
 }
