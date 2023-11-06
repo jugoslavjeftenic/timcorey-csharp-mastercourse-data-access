7c0f840ed9b233aa23f21ba40125c82dcda45c3c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-11-06 21:42:02 +0100

    DataAccessLibrary

--- T40_02_01_DataAccessLibrary/Models/ContactModel.cs
+++ T40_02_01_DataAccessLibrary/Models/ContactModel.cs
@@ -1,10 +1,12 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 
 namespace T40_02_01_DataAccessLibrary.Models
 {
 	public class ContactModel
 	{
 		[BsonId]
+		[JsonProperty(PropertyName = "id")]
 		public Guid Id { get; set; } = Guid.NewGuid();
 		public string FirstName { get; set; } = "";
 		public string LastName { get; set; } = "";
