e110bac4c6f129b019c7f59e9592dfc9751c72df
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-11-09 13:11:10 +0100

    DataAccessLibrary

---
+++ T41_01_01_DataAccessLibrary/Models/ContactModel.cs
@@ -0,0 +1,10 @@
+﻿namespace T41_01_01_DataAccessLibrary.Models
+{
+	public class ContactModel
+	{
+		public string FirstName { get; set; } = "";
+		public string LastName { get; set; } = "";
+		public List<string> PhoneNumbers { get; set; } = new();
+		public List<string> EmailAddresses { get; set; } = new();
+	}
+}
